9ed1dd7810eb98d8c88dca197779a3b9ed25eef9
Ed <as102162597@outlook.com>
2025-01-19 14:32:32 +0800

    [add] configurable via environment variables

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,4 +4,25 @@ const environment = Object.keys(config).includes(process.env.NODE_ENV)
     ? process.env.NODE_ENV
     : 'development';
 
+config[environment].host =
+    process.env.HOST ?? config[environment].host;
+
+config[environment].port =
+    process.env.PORT ?? config[environment].port;
+
+config[environment].dbms.host =
+    process.env.DBMS_HOST ?? config[environment].dbms.host;
+
+config[environment].dbms.port =
+    process.env.DBMS_PORT ?? config[environment].dbms.port;
+
+config[environment].dbms.username =
+    process.env.DBMS_USERNAME ?? config[environment].dbms.username;
+
+config[environment].dbms.password =
+    process.env.DBMS_PASSWORD ?? config[environment].dbms.password;
+
+config[environment].dbms.database =
+    process.env.DBMS_DATABASE ?? config[environment].dbms.database;
+
 export const configuration = config[environment];
